55f11420ece6d488f3a0472e4ee08d5655c4b15d
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2023-09-25 20:57:04 +0200

    usuarios nuevo

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,13 +7,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class HomeController extends AbstractController
+class LoginController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route(name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('', [
+            'last_username' => $lastUsername,
++           'error'         => $error,
         ]);
     }
 }
